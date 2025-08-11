dd1040f823572c45d4d3339ddd7ec73f9b00e08d
Minimini55 <minami25feb@gmail.com>
2025-08-11 14:20:21 +0900

    編集制限のコントローラーの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, except:  [:index, :show]
+  before_action :move_to_root, only: [:edit, :update, :destroy]
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -50,5 +51,10 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy,:concept,:image,).merge(user_id: current_user.id)
   end
+
+   def move_to_root
+    redirect_to root_path unless current_user == @prototype.user
+  end
+  
 end
 
