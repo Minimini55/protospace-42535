927f5886da74bfb126292a1fda5bafb79d044335
Minimini55 <minami25feb@gmail.com>
2025-08-11 10:50:20 +0900

    validatesメソッドの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  validates :profile, presence: true
+  validates :department, presence: true
+  validates :position, presence: true
   has_many :prototypes, dependent: :destroy
   has_many :comment, dependent: :destroy
 end
